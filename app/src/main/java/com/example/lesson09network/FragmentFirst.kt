14b95d0ac30300f7769105794bebe2745482c80d
Serge Labada <sergelabada@gmail.com>
2022-08-26 19:09:33 +0300

    Interim commit

--- app/src/main/java/com/example/lesson09network/FragmentFirst.kt
+++ app/src/main/java/com/example/lesson09network/FragmentFirst.kt
@@ -1,27 +1,20 @@
 package com.example.lesson09network
 
-import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.lesson09network.databinding.FragmentFirstBinding
-import kotlin.random.Random
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class FragmentFirst : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = requireNotNull(_binding)
 
-    private val launcher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) {
-        isGranted ->
-    }
+    private var currentRequest: Call<List<User>>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,15 +29,48 @@ class FragmentFirst : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val githubApi = retrofit.create<GithubApi>()
+
+        currentRequest = githubApi
+            .getUsers(10, 50)
+            .apply {
+                enqueue(object : Callback<List<User>> {
+                    override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                        if (response.isSuccessful) {
+
+                        } else {
+                            handleException(HttpException(response))
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                        if (!call.isCanceled) {
+                            handleException(t)
+                        }
+                        println()
+                    }
+                })
+            }
+
         with(binding) {
             button.setOnClickListener {
-                launcher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+
             }
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        currentRequest?.cancel()
         _binding = null
+    }
+
+    private fun handleException(t: Throwable) {
+
     }
 }
--- app/src/main/java/com/example/lesson09network/MainActivity.kt
+++ app/src/main/java/com/example/lesson09network/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.lesson09network
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.lesson09network.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
