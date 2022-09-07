351080673fc1c4a5fc63f405d669d14f5f0c05f0
Serge Labada <sergelabada@gmail.com>
2022-09-07 19:52:22 +0300

    Processing search menu Toolbar

--- app/src/main/java/com/example/lesson09network/FragmentFirst.kt
+++ app/src/main/java/com/example/lesson09network/FragmentFirst.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -69,12 +71,40 @@ class FragmentFirst : Fragment() {
                 })
             }
         with(binding) {
-            button.setOnClickListener {
+
 //                toolbar.menu
 //                toolbar.inflateMenu(R.menu.menu_toolbar) //for processing menu toolbar
 //                toolbar.setNavigationOnClickListener {
 //                    findNavController().navigateUp() //for processing button "back"
 //                }
+            //for processing click on toolbar menu
+//            toolbar.setOnMenuItemClickListener {
+//                if (it.itemId == R.id.action_search) {
+//                    Toast.makeText(requireContext(), "Hello", Toast.LENGTH_SHORT).show()
+//                    true
+//                } else {
+//                    false
+//                }
+//            }
+            //for processing of search menu toolbar
+//            toolbar
+//                .menu
+//                .findItem(R.id.action_search)
+//                .actionView
+//                .let { it as SearchView }
+//                .setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//                    override fun onQueryTextSubmit(query: String?): Boolean {
+//                        TODO("Not yet implemented")
+//                    }
+//
+//                    override fun onQueryTextChange(newText: String?): Boolean {
+//                        TODO("Not yet implemented")
+//                    }
+//                })
+
+            button.setOnClickListener {
+
+
             }
         }
     }
